7b6a58f1615290d4edefadee33ecd6ed01bfa0d6
behnamzarei20-arch <behnam.zarei20@gmail.com>
2026-09-10 22:52:31 +0330

    Configure Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bbberah/',
   plugins: [react()],
   resolve: {
     alias: {
